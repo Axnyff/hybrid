833a6701748fb75d1d2388a343b40c93e33079d6
Axel Nyffenegger <Axnyff@gmail.com>
2018-11-11 19:50:41 +0100

    Webpack

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -186,7 +186,6 @@ module.exports = {
             options: {
               formatter: require.resolve('react-dev-utils/eslintFormatter'),
               eslintPath: require.resolve('eslint'),
-              
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -202,7 +201,7 @@ module.exports = {
           // smaller than specified limit in bytes as data URLs to avoid requests.
           // A missing `test` is equivalent to a match.
           {
-            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.mp3$/],
             loader: require.resolve('url-loader'),
             options: {
               limit: 10000,
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -275,7 +275,7 @@ module.exports = {
           // "url" loader works just like "file" loader but it also embeds
           // assets smaller than specified size as data URLs to avoid requests.
           {
-            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.mp3/],
             loader: require.resolve('url-loader'),
             options: {
               limit: 10000,
